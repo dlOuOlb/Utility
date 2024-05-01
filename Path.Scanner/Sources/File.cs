b896fab887ff12814c06f4077f659f69804e9ce5
dlOuOlb <iuns@kakao.com>
2024-05-01 23:03:17 +0900

    Version Update - Path.Scanner 0.1.0.0
    - Error messages about the files have been improved.
    - Directories are now printed in the registered order.
    - Filenames are now printed in the alphanumeric order.

--- Path.Scanner/Sources/File.cs
+++ Path.Scanner/Sources/File.cs
@@ -1,6 +1,7 @@
 ﻿using S = System;
 using G = System.Collections.Generic;
-using static System.Linq.ParallelEnumerable;
+using static System.Linq.Enumerable;
+using P = System.Linq.ParallelEnumerable;
 
 namespace Utility
 {
@@ -11,18 +12,17 @@ namespace Utility
 			private static readonly S.Boolean isCaseSensitive = @"." != S.IO.Path.GetRelativePath( relativeTo: "A", path: "a" );
 			private static readonly S.StringComparer comparer = isCaseSensitive ? S.StringComparer.InvariantCulture : S.StringComparer.InvariantCultureIgnoreCase;
 
-			private static Value Get<Value>( Value value ) => value;
 			private static S.String GetName( S.IO.FileSystemInfo information ) => information.Name;
 
-			private static G.SortedSet<S.String>? Scan( S.String directory ) => S.IO.Directory.Exists( path: directory ) ? new( comparer: comparer, collection: new S.IO.DirectoryInfo( path: directory ).EnumerateFiles( ).AsParallel( ).Select( selector: GetName ) ) : null;
-			public static G.SortedDictionary<S.String, G.SortedSet<S.String>?> Scan( G.IEnumerable<S.String> directories ) => new( comparer: comparer, dictionary: directories.AsParallel( ).ToDictionary( comparer: comparer, keySelector: Get, elementSelector: Scan ) );
+			private static G.KeyValuePair<S.String, G.SortedSet<S.String>?> Scan( S.String directory ) => new( key: directory, value: S.IO.Directory.Exists( path: directory ) ? new( comparer: comparer, collection: new S.IO.DirectoryInfo( path: directory ).EnumerateFiles( ).Select( selector: GetName ) ) : null );
+			public static G.IEnumerable<G.KeyValuePair<S.String, G.SortedSet<S.String>?>> Scan( G.IEnumerable<S.String> directories ) => directories.Select( selector: Scan );
 
-			public static S.Linq.ParallelQuery<S.String> Intersect( G.IEnumerable<S.String> former, G.IEnumerable<S.String> latter ) => former.AsParallel( ).Intersect( second: latter.AsParallel( ), comparer: comparer );
+			public static G.IEnumerable<S.String> Intersect( G.IEnumerable<S.String> former, G.IEnumerable<S.String> latter ) => former.Intersect( second: latter, comparer: comparer );
 		}
 
 		public static class Data: S.Object
 		{
-			private static S.Boolean Equals<Value>( G.IEnumerable<Value> former, G.IEnumerable<Value> latter ) => former.AsParallel( ).SequenceEqual( second: latter.AsParallel( ) );
+			private static S.Boolean Equals<Value>( G.IEnumerable<Value> former, G.IEnumerable<Value> latter ) => P.SequenceEqual( first: P.AsParallel( source: former ), second: P.AsParallel( source: latter ) );
 			private static S.Boolean Equals( S.String former, S.String latter ) => Equals( former: S.IO.File.ReadAllBytes( path: former ), latter: S.IO.File.ReadAllBytes( path: latter ) );
 			public static S.Boolean Equals( S.IO.FileInfo former, S.IO.FileInfo latter )
 			{
--- Path.Scanner/Sources/Program.Verify.cs
+++ Path.Scanner/Sources/Program.Verify.cs
@@ -8,6 +8,13 @@ namespace Utility
 	{
 		private static readonly G.HashSet<S.String> extensions = [ @".dll", @".exe" ];
 
+		private static S.Exception? Verify( S.IO.FileInfo argument, S.IO.FileInfo variable )
+		{
+			try { return File.Data.Equals( former: argument, latter: variable ) ? null : throw new S.Exception( message: @"Different file contents." ); }
+			catch( S.Exception exception ) { return exception; }
+			finally { }
+		}
+
 		private static void Verify( G.IEnumerable<S.String> arguments, G.IEnumerable<S.String> variables )
 		{
 			var directories = new { arguments = File.Name.Scan( directories: arguments ), variables = File.Name.Scan( directories: variables ) };
@@ -29,16 +36,21 @@ namespace Utility
 							Console.Print( message: $"\t- \"{variable.Key}\"", significant: names.Any( ) );
 
 							foreach( var name in names )
-								Console.Print
-								(
-									message: $"\t\t- \"{name}\"",
-									significant: extensions.Contains( item: S.IO.Path.GetExtension( path: name ).ToLowerInvariant( ) ),
-									error: !File.Data.Equals
-									(
-										former: new S.IO.FileInfo( fileName: S.IO.Path.Combine( path1: argument.Key, path2: name ) ),
-										latter: new S.IO.FileInfo( fileName: S.IO.Path.Combine( path1: variable.Key, path2: name ) )
-									)
-								);
+							{
+								var significant = extensions.Contains( item: S.IO.Path.GetExtension( path: name ).ToLowerInvariant( ) );
+								var file = new
+								{
+									argument = new S.IO.FileInfo( fileName: S.IO.Path.Combine( path1: argument.Key, path2: name ) ),
+									variable = new S.IO.FileInfo( fileName: S.IO.Path.Combine( path1: variable.Key, path2: name ) )
+								};
+
+								if( Verify( argument: file.argument, variable: file.variable ) is S.Exception exception )
+									Console.Print( message: $"\t\t- \"{name}\": {exception.Message}", significant: significant, error: true );
+								else
+									Console.Print( message: $"\t\t- \"{name}\"", significant: significant );
+
+								continue;
+							}
 
 							continue;
 						}
